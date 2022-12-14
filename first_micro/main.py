215f9e0f0bcdc9e2d7ef52ad357718b76a799377
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-14 13:57:26 +0330

    first commit

---
+++ first_micro/main.py
@@ -0,0 +1,19 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def Hello_World():
+    return"HELLO WORLD..."
+
+@app.route('/<user>', methods=["GET"])
+def get_username(user):
+    return f"<html><body> Hello {user}</body></html>" 
+
+@app.route('/text', methods=["GET"])
+def get_text():
+    return render_template("text.html")
+
+@app.route('/product/<product_id>', methods=["GET"])
+def get_product_info(product_id):
+    return f"<html><body> Product_Code : {product_id}</body></html>"  
