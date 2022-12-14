4b01ec5a6c96cfff96513e43be71751241c80c22
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-14 16:44:44 +0330

    second commit

--- first_micro/main.py
+++ first_micro/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def Hello_World():
-    return"HELLO WORLD..."
+    return"HELLO WORLD github actions..."
 
 @app.route('/<user>', methods=["GET"])
 def get_username(user):
