25ceb602d182f507a4fcc0826a05737aa562f531
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-14 20:31:35 +0330

    add hello world

--- first_micro/main.py
+++ first_micro/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def Hello_World():
-    return"HELLO WORLD git ..."
+    return"HELLO WORLD git action ..."
 
 @app.route('/<user>', methods=["GET"])
 def get_username(user):
